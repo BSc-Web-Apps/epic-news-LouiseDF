60d5e79f4bef24090c6b0c9e4bbc65dcd7a4d6fe
LouiseDF <LD114160@hc.ac.je>
2025-11-06 12:10:23 +0000

    Update root.tsx
    begin team member card

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import Document from './components/shared-layout/Document.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
+import portrait1 from '~/assets/jpg/portrait-01.jpg'
 
 export const links: Route.LinksFunction = () => {
 	return rootLinkElements
@@ -21,7 +22,13 @@ export default function App() {
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">Welcome to Epic News!</h1>
+						<h1 className="text-mega">Your Journey Begins!</h1>
+						<p className="text-base text-gray-200 md:text-lg lg:text-xl">
+							Welcome to Epic News, where the latest developments in tech are
+							found.
+						</p>
+						<img src={portrait1} alt="Portraitn 1"></img>
+						<p>Michael Angelo</p>
 					</main>
 				</div>
 			</div>
