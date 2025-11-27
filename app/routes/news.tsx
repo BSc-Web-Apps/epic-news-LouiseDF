d12400c30c5056a36aaa0b351ab685d64f033bce
LouiseDF <LD114160@hc.ac.je>
2025-11-27 11:15:57 +0000

    added labels on news

--- app/routes/news.tsx
+++ app/routes/news.tsx
@@ -1,7 +1,54 @@
-export default function AboutUsRoute() {
+import { NavLink, Outlet } from 'react-router'
+
+export default function NewsPage() {
 	return (
-		<main className="container py-16">
-			<h1 className="text-mega">Latest News</h1>
+		<main className="flex flex-col py-24">
+			<div className="container">
+				<h1 className="text-h1">Latest News</h1>
+				<div className="flex space-x-4 py-4">
+					<NavLink
+						to="local news"
+						prefetch="intent"
+						className={({ isActive }) =>
+							`${isActive ? 'text-blue-300 underline underline-offset-8' : 'text-blue-500 transition-colors hover:text-yellow-300 sm:inline-block'}`
+						}
+					>
+						Local News
+					</NavLink>
+
+					<NavLink
+						to="sport"
+						prefetch="intent"
+						className={({ isActive }) =>
+							`${isActive ? 'text-blue-300 underline underline-offset-8' : 'text-blue-500 transition-colors hover:text-yellow-300 sm:inline-block'}`
+						}
+					>
+						Sport
+					</NavLink>
+
+					<NavLink
+						to="health"
+						prefetch="intent"
+						className={({ isActive }) =>
+							`${isActive ? 'text-blue-300 underline underline-offset-8' : 'text-blue-500 transition-colors hover:text-yellow-300 sm:inline-block'}`
+						}
+					>
+						Health
+					</NavLink>
+
+					<NavLink
+						to="travel"
+						prefetch="intent"
+						className={({ isActive }) =>
+							`${isActive ? 'text-blue-300 underline underline-offset-8' : 'text-blue-500 transition-colors hover:text-yellow-300 sm:inline-block'}`
+						}
+					>
+						Travel
+					</NavLink>
+				</div>
+			</div>
+
+			<Outlet />
 		</main>
 	)
 }
