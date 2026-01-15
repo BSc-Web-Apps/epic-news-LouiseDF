e77ec6ca8bdd2139662df42f9603b5e3dbe521ac
LouiseDF <LD114160@hc.ac.je>
2026-01-15 12:28:49 +0000

    added different categories to the news, moved team member cards to about us page

--- app/components/organisms/ArticleCard.tsx
+++ app/components/organisms/ArticleCard.tsx
@@ -13,7 +13,7 @@ export default function ArticleCard({
 }: ArticleCardProps) {
 	return (
 		<Link to={`/article/${articleId}`}>
-			<div className="rounded-lg bg-blue-900 p-4 transition-all hover:scale-105">
+			<div className="h-40 rounded-lg bg-blue-900 p-4 transition-all hover:scale-105">
 				<h3>{title}</h3>
 				<p>{category || 'General News'}</p>
 			</div>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,6 @@
-import { RiLinkedinBoxFill, RiTwitterXFill } from 'react-icons/ri'
 import { data, type MetaFunction, useLoaderData } from 'react-router'
 import HeroCallToAction from '#app/components/organisms/Hero/HeroCallToAction.tsx'
-import portrait1 from '~/assets/jpg/portrait-01.jpg'
-import portrait2 from '~/assets/jpg/portrait-02.jpg'
-import portrait3 from '~/assets/jpg/portrait-03.jpg'
+
 import hero from '~/assets/jpg/sample-hero.jpg'
 import ArticleCard from '~/components/organisms/ArticleCard.tsx'
 import { prisma } from '~/utils/db.server.ts'
@@ -11,7 +8,10 @@ import { prisma } from '~/utils/db.server.ts'
 export const meta: MetaFunction = () => [{ title: 'Epic News' }]
 
 export async function loader() {
-	const allArticles = await prisma.article.findMany({
+	const generalArticles = await prisma.article.findMany({
+		where: {
+			isPublished: true,
+		},
 		select: {
 			id: true,
 			title: true,
@@ -20,39 +20,65 @@ export async function loader() {
 		},
 	})
 
-	return data({ allArticles })
-}
+	const techArticles = await prisma.article.findMany({
+		where: {
+			isPublished: true,
+			category: { slug: 'technology' },
+		},
+		select: {
+			id: true,
+			title: true,
+			category: { select: { name: true } },
+			images: { select: { objectKey: true } },
+		},
+	})
 
-interface TeamMemberCardProps {
-	name: string
-	role: string
-	imageSrc: string
-}
+	const entertainmentArticles = await prisma.article.findMany({
+		where: {
+			isPublished: true,
+			category: { slug: 'entertainment' },
+		},
+		select: {
+			id: true,
+			title: true,
+			category: { select: { name: true } },
+			images: { select: { objectKey: true } },
+		},
+	})
 
-export function TeamMemberCard({ name, role, imageSrc }: TeamMemberCardProps) {
-	return (
-		<div className="w-fit rounded-lg bg-slate-800 p-8">
-			<img
-				src={imageSrc}
-				alt="An employee"
-				className="mx-auto h-64 w-64 rounded-full"
-			/>
+	const buisnessArticles = await prisma.article.findMany({
+		where: {
+			isPublished: true,
+			category: { slug: 'business' },
+		},
+		select: {
+			id: true,
+			title: true,
+			category: { select: { name: true } },
+			images: { select: { objectKey: true } },
+		},
+	})
 
-			<div className="pt-6">
-				<h3 className="text-center font-semibold text-white">{name}</h3>
-				<p className="pt-1 text-center text-slate-400">{role}</p>
-
-				<div className="flex justify-center gap-4 pt-6 text-slate-400">
-					<RiTwitterXFill />
-					<RiLinkedinBoxFill />
-				</div>
-			</div>
-		</div>
-	)
+	return data({
+		generalArticles,
+		techArticles,
+		entertainmentArticles,
+		buisnessArticles,
+	})
 }
 
 export default function Index() {
-	const { allArticles } = useLoaderData<typeof loader>()
+	const {
+		generalArticles,
+		techArticles,
+		entertainmentArticles,
+		buisnessArticles,
+	} = useLoaderData<typeof loader>()
+
+	const hasGeneralArticles = generalArticles.length > 0
+	const hasTechArticles = techArticles.length > 0
+	const hasEntertainmentArticles = entertainmentArticles.length > 0
+	const hasBuisnessArticles = buisnessArticles.length > 0
 
 	return (
 		<main className="grid h-full place-items-center">
@@ -69,47 +95,81 @@ export default function Index() {
 				</HeroCallToAction>
 			</div>
 
-			<div className="m-4 flex gap-8">
-				<TeamMemberCard
-					name="Leonard"
-					role="Senior Developer"
-					imageSrc={portrait1}
-				/>
-
-				<TeamMemberCard
-					name="John Smith"
-					role="Lead Developer"
-					imageSrc={portrait2}
-				/>
-
-				<TeamMemberCard
-					name="Jane-Doe"
-					role="Lead Developer"
-					imageSrc={portrait3}
-				/>
-			</div>
 			<div className="container py-16">
-				<h2 className="text-h2 mb-8 font-normal">Latest news</h2>
+				<h2 className="text-h2 mb-8 font-normal">General</h2>
+			</div>
+			<div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+				{hasGeneralArticles ? (
+					generalArticles.map((article) => {
+						return (
+							<div key={article.id} className="col-span-2 row-span-2">
+								<ArticleCard
+									articleId={article.id}
+									title={article.title}
+									category={article.category?.name}
+								/>
+							</div>
+						)
+					})
+				) : (
+					<p>No articles found</p>
+				)}
+			</div>
+
+			<div className="container py-16">
+				<h2 className="text-h2 mb-8 font-normal">Technology</h2>
 
 				<div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
-					{allArticles.length > 0 ? (
-						allArticles.map((article, index) => {
-							return index === 0 ? (
-								<div className="col-span-2 row-span-2">
+					{hasTechArticles ? (
+						techArticles.map((article) => {
+							return (
+								<div key={article.id} className="col-span-2 row-span-2">
 									<ArticleCard
-										key={article.id}
 										articleId={article.id}
 										title={article.title}
 										category={article.category?.name}
 									/>
 								</div>
-							) : (
-								<ArticleCard
-									key={article.id}
-									articleId={article.id}
-									title={article.title}
-									category={article.category?.name}
-								/>
+							)
+						})
+					) : (
+						<p>No articles found</p>
+					)}
+				</div>
+
+				<h2 className="text-h2 mb-8 font-normal">Entertainment</h2>
+
+				<div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+					{hasEntertainmentArticles ? (
+						entertainmentArticles.map((article) => {
+							return (
+								<div key={article.id} className="col-span-2 row-span-2">
+									<ArticleCard
+										articleId={article.id}
+										title={article.title}
+										category={article.category?.name}
+									/>
+								</div>
+							)
+						})
+					) : (
+						<p>No articles found</p>
+					)}
+				</div>
+
+				<h2 className="text-h2 mb-8 font-normal">Buisness</h2>
+
+				<div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+					{hasBuisnessArticles ? (
+						buisnessArticles.map((article) => {
+							return (
+								<div key={article.id} className="col-span-2 row-span-2">
+									<ArticleCard
+										articleId={article.id}
+										title={article.title}
+										category={article.category?.name}
+									/>
+								</div>
 							)
 						})
 					) : (
